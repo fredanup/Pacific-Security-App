7aefb0a4b26b1facb7e44c5992cc77ea6b71e5d2
Angel Ugarte <fredanup@hotmail.com>
2024-06-18 10:31:10 -0500

    Corrección de desbordamiento de tabla de documentos

--- src/pages/dashboard/profile.tsx
+++ src/pages/dashboard/profile.tsx
@@ -2,29 +2,31 @@ import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import FormTitle from 'pages/utilities/form-title';
 import Layout from 'pages/utilities/layout';
+import { trpc } from 'utils/trpc';
 
 export default function Profile() {
-  const { data: session } = useSession();
+  //Obtener el usuario actual
+  const { data: currentUser } = trpc.user.findCurrentOne.useQuery();
   return (
     <>
       <Layout>
         <FormTitle text="Perfil" />
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 w-full h-full">
           {/*Foto y datos personales*/}
-          <div className="flex flex-col items-center p-4">
+          <div className="flex flex-col items-center py-4 ">
             <Image
               className="rounded-full"
-              src={session?.user?.image || ''}
+              src={currentUser?.image || ''}
               width={95}
               height={100}
               alt="Logo"
             />
             <p className="text-m text-base font-medium text-gray-700">
-              {session?.user?.name}
+              {currentUser?.name}
             </p>
             <div className="flex flex-row gap-2 items-center">
               <p className="text-sm font-normal text-gray-500">
-                {session?.user?.email}
+                {currentUser?.email}
               </p>
               {/**Este es el botón de edición */}
               <svg
@@ -35,8 +37,201 @@ export default function Profile() {
               </svg>
             </div>
           </div>
-          <h3 className="text-black text-sm font-medium">Datos personales</h3>
-          <h3 className="text-black text-sm font-medium">Documentos</h3>
+          <div className="py-4 border-b border-gray-200">
+            <h3 className="text-black text-sm font-medium">Datos personales</h3>
+            <ul className="list-disc pl-5">
+              <li className="text-gray-500 text-sm">
+                Nombres: {currentUser?.name}
+              </li>
+              <li className="text-gray-500 text-sm">
+                Apellidos: {currentUser?.lastName}
+              </li>
+            </ul>
+          </div>
+
+          <h3 className="text-black text-sm font-medium">Expediente</h3>
+          <div className="h-full w-full overflow-x-auto pb-12 md:pb-0">
+            <table className="h-full w-full">
+              <thead className="border-b border-gray-200 text-left text-black text-sm font-light">
+                <tr>
+                  <th className="py-4 pr-2">Nro.</th>
+                  <th className="py-4 pr-2">Documento</th>
+                  <th className="py-4 pr-2">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="border-b border-gray-200 text-sm font-light">
+                  <td className="py-4 pr-2">1</td>
+                  <td className="py-4 pr-2">DNI</td>
+                  <td className="py-4 ">
+                    <svg
+                      viewBox="0 0 448 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z" />
+                    </svg>
+                  </td>
+                  <td className="py-4">
+                    <svg
+                      viewBox="0 0 512 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 text-sm font-light">
+                  <td className="py-4 pr-2">2</td>
+
+                  <td className="py-4 pr-2">Carné SUCAMEC</td>
+                  <td className="py-4 ">
+                    <svg
+                      viewBox="0 0 448 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z" />
+                    </svg>
+                  </td>
+                  <td className="py-4">
+                    <svg
+                      viewBox="0 0 512 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 text-sm font-light">
+                  <td className="py-4 pr-2">3</td>
+
+                  <td className="py-4 pr-2">Licencia para portar armas</td>
+                  <td className="py-4 ">
+                    <svg
+                      viewBox="0 0 448 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z" />
+                    </svg>
+                  </td>
+                  <td className="py-4">
+                    <svg
+                      viewBox="0 0 512 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 text-sm font-light">
+                  <td className="py-4 pr-2">4</td>
+                  <td className="py-4 pr-2">
+                    Antecedentes (penales, policiales y judiciales) o
+                    Certificado único laboral
+                  </td>
+                  <td className="py-4 ">
+                    <svg
+                      viewBox="0 0 448 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z" />
+                    </svg>
+                  </td>
+                  <td className="py-4">
+                    <svg
+                      viewBox="0 0 512 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 text-sm font-light">
+                  <td className="py-4 pr-2">5</td>
+                  <td className="py-4 pr-2">
+                    Certificado de capacitación SUCAMEC
+                  </td>
+                  <td className="py-4 ">
+                    <svg
+                      viewBox="0 0 448 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z" />
+                    </svg>
+                  </td>
+                  <td className="py-4">
+                    <svg
+                      viewBox="0 0 512 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 text-sm font-light">
+                  <td className="py-4 pr-2">6</td>
+                  <td className="py-4 pr-2">
+                    Certificado físico y psicológico
+                  </td>
+                  <td className="py-4 ">
+                    <svg
+                      viewBox="0 0 448 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z" />
+                    </svg>
+                  </td>
+                  <td className="py-4">
+                    <svg
+                      viewBox="0 0 512 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 text-sm font-light">
+                  <td className="py-4 pr-2">7</td>
+                  <td className="py-4 pr-2">Certificado de estudios</td>
+                  <td className="py-4 ">
+                    <svg
+                      viewBox="0 0 448 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z" />
+                    </svg>
+                  </td>
+                  <td className="py-4">
+                    <svg
+                      viewBox="0 0 512 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 text-sm font-light">
+                  <td className="py-4 pr-2">8</td>
+                  <td className="py-4 pr-2">Certificado laboral</td>
+                  <td className="py-4 ">
+                    <svg
+                      viewBox="0 0 448 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z" />
+                    </svg>
+                  </td>
+                  <td className="py-4">
+                    <svg
+                      viewBox="0 0 512 512"
+                      className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
+                    >
+                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </Layout>
     </>
